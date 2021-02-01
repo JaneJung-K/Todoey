311336468fe8e9da7ff4d06fa7c3ade58afa7466
JaneJung <piaojane@daum.net>
2021-02-01 13:33:41 +0900

    Make Category View Controller and Relationship Graphs in Core Data

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by mac on 2021/02/01.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+   
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+}
